b38b05b10035465303ed8181554e0a32b50076bd
dell <jmsugay11@gmail.com>
2020-04-19 12:15:37 +0800

    deploying app

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ mongoose
 app.use('/api/items',items);
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production'){
     //Set Static folder
     app.use(express.static('client/build'));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
